a33ef1afe5db7fcc261ce01943fce13595bd851e
Erika Yi <yi.erika2@gmail.com>
2021-08-27 16:47:18 -0400

    initial commit - add files and folders

---
+++ PyParagraph/paragraph.py
@@ -0,0 +1,16 @@
+import os
+
+# Import a text file filled with a paragraph of your choosing.
+first_paragraph_txt = "raw_data/paragraph_1.txt"
+second_paragraph_txt = "raw_data/paragraph_2.txt"
+
+# Assess the passage for each of the following:
+
+#   Approximate word count
+
+#   Approximate sentence count
+
+#   Approximate letter count (per word)
+
+#   Average sentence length (in words)
+
