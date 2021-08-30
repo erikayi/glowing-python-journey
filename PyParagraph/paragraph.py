50fbe43aae746819ccc6a8af6219b183e6f37a36
Erika Yi <yi.erika2@gmail.com>
2021-08-29 23:47:23 -0400

    initial commit for paragraph data

--- PyParagraph/paragraph.py
+++ PyParagraph/paragraph.py
@@ -5,10 +5,15 @@ first_paragraph_txt = "raw_data/paragraph_1.txt"
 second_paragraph_txt = "raw_data/paragraph_2.txt"
 
 # Assess the passage for each of the following:
+with open(first_paragraph_txt, 'r'):
 
 #   Approximate word count
-
+    firstparagraph_word_count = 0
+    for row in first_paragraph_txt:
+        firstparagraph_word_count += 1
+    
 #   Approximate sentence count
+    firstparagraph_sentence_count = 0
 
 #   Approximate letter count (per word)
 
