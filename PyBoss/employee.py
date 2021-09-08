9c17b6cbd888344b9c4049bc56e49d224840aaaa
Erika Yi <yi.erika2@gmail.com>
2021-09-08 15:51:39 -0400

    add files, clean files

--- PyBoss/employee.py
+++ PyBoss/employee.py
@@ -6,39 +6,20 @@ import os
 employee_data = os.path.join("data/employee_data.csv")
 
 # open the data 
-with open('data/employee_data.csv', 'r') as csvfile:
+with open(employee_data, 'r') as csvfile:
     reader = csv.reader(csvfile)
 
     # parse csv data in python
-    # for row in reader:
-    #     print(row)
-    # csvfile.close()
-
-with open('data/employee_data.csv', 'r') as splitnames:
-    reader = csv.reader(splitnames, delimiter=',')
-    newcsvdict = {"First Name": [], "Last Name": []}
     for row in reader:
-        first = row[0].split(" ")[0]
-        last = row[0].split(" ")[1]
-        newcsvdict['First Name'].append(first)
-        newcsvdict['Last Name'].append(last)
         print(row)
+
+# with open('data/employee_data.csv', 'r') as splitnames:
+#     reader = csv.reader(splitnames, delimiter=',')
+#     newcsvdict = {"First Name": [], "Last Name": []}
+#     for row in reader:
+#         first = row[0].split(" ")[0]
+#         last = row[0].split(" ")[1]
+#         newcsvdict['First Name'].append(first)
+#         newcsvdict['Last Name'].append(last)
+#         print(row)
        
-
-# with open('new_employee_data.csv') as splitnames:
-#     write = csv.DictWriter(splitnames, newcsvdict.keys())
-#     write.writeheader()
-#     write.writerows(newcsvdict)
-
-# with open(employee_data, 'r') as employeecsv:
-#     csvreader = csv.reader(employeecsv)
-#     next(csvreader)
-#     last_name = [row[1].split(" ")[1] for row in csvreader]
-
-# add splitted data in the new dataset
-
-# with open('new_employee_data.csv', 'wb') as employeecsv:
-#     csvwrite = csv.DictWriter(employeecsv, newcsvdict.keys())
-#     csvwrite.writeheader()
-#     csvwrite.writerows(newcsvdict)
-
